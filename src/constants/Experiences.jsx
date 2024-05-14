e656516841965991c1a638b85371c82c19ebae4d
Robert Medina <csoperativo@gmail.com>
2024-05-14 00:12:21 -1000

    fix dummy Constants

---
+++ src/constants/Experiences.jsx
@@ -0,0 +1,45 @@
+import { BM, trabajardesdecasa, techsolution } from "../assets/images";
+export const Experiences = [
+  {
+    title: "Front-end Developer",
+    company_name: "BmBot",
+    country: "Venezuela - Caracas",
+    icon: BM,
+    iconBg: "#accbe1",
+    date: "September 2022 - March 2023",
+    points: [
+      "Developing and maintaining web applications using React.js, blockchains and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Freelancer",
+    country: "Chile - Puerto Varas",
+    company_name: "VisibilityTech",
+    icon: trabajardesdecasa,
+    iconBg: "#fbc3bc",
+    date: "January 2021 - june 2022",
+    points: [
+      "Developing and maintaining web applications using Javascript and other related technologies.",
+      "Conducted testing on web pages, analyzed user feedback, and coordinated with cross-functional teams to identify opportunities to enhance web page responsiveness.",
+      "Developed prototypes, user interface designs, and wireframes for the company website",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Control and logistics coordinator",
+    company_name: "Techsolution",
+    country: "Chile - Puerto Varas",
+    icon: techsolution,
+    iconBg: "#b7e4c7",
+    date: "April 2019 - April 2021",
+    points: [
+      "As a Full Stack Web Developer, my experience includes being the general coordinator of all technicians at the national level and a project developer. I was in charge of the elaboration and implementation of projects in different regions, including the creation of web servers, networks, and programs.",
+      "Coordinating and leading a team of technicians at the national level to ensure successful project implementation in different regions.",
+      "I developed and implemented web server and network solutions that improved operational efficiency and information security at the national level",
+      "I led the creation of custom programs that optimized operational processes in various regions, resulting in increased productivity and efficiency.",
+    ],
+  },
+];
---
+++ src/constants/Skills.jsx
@@ -0,0 +1,100 @@
+import {
+  css,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  react,
+  redux,
+  sass,
+  tailwindcss,
+  typescript,
+} from "../assets/images";
+export const Skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: motion,
+    name: "Motion",
+    type: "Animation",
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+];
