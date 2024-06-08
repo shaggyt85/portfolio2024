f706d3e49e1cf521de1627de5904b70486f577ab
Robert Medina <csoperativo@gmail.com>
2024-06-07 22:43:06 -1000

    chore: Update homepage URL in package.json and add swipe navigation message in ContentData.jsx

--- src/components/Home/ContentData.jsx
+++ src/components/Home/ContentData.jsx
@@ -5,10 +5,18 @@ export const ContentData = [
   {
     id: 1,
     component: (
-      <h1 className="sm:text-ml sm:leading-snug text-center neo-brutalism-blue py-2 px-4 text-white mx-5">
-        Hi, I am <span className="font-bold">Robert Medina</span> 👋
-        <br />A Front-End Developer.
-      </h1>
+      <div className="flex flex-col  gap-64 sm:gap-96">
+        <h1 className="sm:text-ml sm:leading-snug text-center neo-brutalism-blue py-2 px-4 text-white mx-5">
+          Hi, I am <span className="font-bold">Robert Medina</span> 👋
+          <br />A Front-End Developer.
+        </h1>
+        <h2 className="sm:text-ml sm:leading-snug text-center neo-brutalism-blue py-2 px-4 text-white mx-5">
+          You can navigate using the arrow keys{" "}
+          <span className="font-bold">👈 👉</span>
+          <br />
+          by swiping the screen to the <span className="font-bold"> 👈 👉</span>
+        </h2>
+      </div>
     ),
   },
   {
@@ -39,6 +47,17 @@ export const ContentData = [
         link="/contact"
         btnText="Let's talk"
       />
+    ),
+  },
+  {
+    id: 5,
+    component: (
+      <h2 className="sm:text-ml sm:leading-snug text-center neo-brutalism-blue py-2 px-4 text-white mx-5">
+        You can navigate using the arrow keys{" "}
+        <span className="font-bold">👈 👉</span> 👋
+        <br />
+        by swiping the screen to the <span className="font-bold"> 👈 👉</span>
+      </h2>
     ),
   },
 ];
